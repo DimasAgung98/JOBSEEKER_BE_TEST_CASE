83d6f60b8ae825bf1513bef3fffe909bcfd1bbbd
dimas agung <dimasraden0798@gmail.com>
2022-08-27 16:05:56 +0800

    Read Database

--- controller/goods.js
+++ controller/goods.js
@@ -1,23 +1,45 @@
-const {
-    append
-} = require('express/lib/response');
+const { append } = require('express/lib/response');
 const res = require('express/lib/response');
-const {
-    Goods
-} = require('../models');
+const { Goods } = require('../models');
 
-function getData(request, response) {
+const datatable = require('sequelize-datatable');
+
+function getData(req, res) {
     Goods.findAll({
 
     })
         .then(goodsList => {
-            response.render('goods', {
+            res.render('goods', {
                 goodsList
             })
         });
 }
 
+function getDatav2(req, res) {
+    Goods.findAll({
+
+    })
+        .then(goodsList => {
+            res.render('goodsv2', {
+                goodsList
+            })
+        });
+}
+
+function getGoodsData(req, res) {
+    // Goods.findAll().then((data) => {
+    //     res.json(data);
+    // }
+    // )
+    datatable(Goods, req.query, {})
+        .then((result) => {
+            // result is response for datatables
+            res.json(result);
+        });
+}
 //Export Module to route.js
 module.exports = {
-    getData
+    getData,
+    getGoodsData,
+    getDatav2
 }
--- routes/router.js
+++ routes/router.js
@@ -8,6 +8,9 @@ const goodsController = require('../controller/goods');
 
 //ROUTE
 router.get('/', goodsController.getData);
+router.get('/data', goodsController.getGoodsData);
+router.get('/show', goodsController.getDatav2);
+
 
 //EXPORT MODULE ROUTER
 module.exports = router;
